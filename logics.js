a0ed37303a1aa31becb5710741da532f822bc398
farrukh malik <farrukh.malik@sleepwellsystem.com>
2019-06-29 00:52:52 -0700

    update code and add functionality

--- logics.js
+++ logics.js
@@ -1,149 +1,139 @@
-var yourAmount;
+// your pocket amount
+var yourAmount; 
+// your total buy amount
 var totalBuy = 0;
-
+// total seprate price of each fruit
 var totalApplePrice = 0;
 var totalOrangePrice = 0;
 var totalBananaPrice = 0;
-
+// actual price of fruits
 var applePrice = 10;
 var orangePrice = 15;
 var bananaPrice = 7;
+// for increment and desrement
+var appleFlag = 0;
+var orangeFlag = 0;
+var bananaFlag = 0;
 
 // seprate apple
 function applePriceAdd(){
     yourAmount =  document.querySelector('.yourAmountInput').value;
 
-    console.log('yourAmount '+yourAmount)
     if(yourAmount == 0){
-        console.log('you dont have money');
+        alert('   You Dont Have Money   ');
     }else{
+        appleFlag++;
     totalApplePrice = totalApplePrice + applePrice; 
     totalBuy = totalBuy + applePrice;
 
-    console.log('added '+totalApplePrice)
-    console.log('added '+totalBuy)
-
     if(totalBuy>yourAmount){
+        appleFlag--;
         totalApplePrice = totalApplePrice - applePrice; 
         totalBuy = totalBuy - applePrice;
-
-        console.log('minus '+totalApplePrice)
-        console.log('minus '+totalBuy)
-        console.log('you cross the limit')
+        alert('   You Cross The Limit   ')
         }
     }
+    document.querySelector('.totalBuy').innerHTML = totalBuy;
+    document.querySelector('.appleFlag').value = appleFlag;
 }
 
 
 function applePriceMinus(){
     yourAmount =  document.querySelector('.yourAmountInput').value;
 
-    console.log('yourAmount '+yourAmount)
     if(yourAmount == 0){
-        console.log('you dont have money');
+        alert('   You Dont Have Money   ');
     }else if(totalApplePrice < 10){
-        console.log('you dont have apple money');
+        alert('   You Dont Have Apple   ');
     }
     else{
+        appleFlag--;
     totalApplePrice = totalApplePrice - applePrice; 
     totalBuy = totalBuy - applePrice;
-
-    console.log('minusApple '+totalApplePrice)
-    console.log('minusApple '+totalBuy)
 }
-}
+document.querySelector('.totalBuy').innerHTML = totalBuy;
+document.querySelector('.appleFlag').value = appleFlag;
 
-// seprate
+}
 
 // seprate orange
 function orangePriceAdd(){
     yourAmount =  document.querySelector('.yourAmountInput').value;
 
-    console.log('yourAmount '+yourAmount)
     if(yourAmount == 0){
-        console.log('you dont have money');
+        alert('   You Dont Have Money   ');
     }else{
+        orangeFlag++;
     totalOrangePrice = totalOrangePrice + orangePrice; 
     totalBuy = totalBuy + orangePrice;
 
-    console.log('added '+totalOrangePrice)
-    console.log('added '+totalBuy)
-
     if(totalBuy>yourAmount){
+        orangeFlag--;
         totalOrangePrice = totalOrangePrice - orangePrice; 
         totalBuy = totalBuy - orangePrice;
-
-        console.log('minus '+totalOrangePrice)
-        console.log('minus '+totalBuy)
-        console.log('you cross the limit')
+        alert('   You Cross The Limit   ')
         }
     }
+    document.querySelector('.totalBuy').innerHTML = totalBuy;
+    document.querySelector('.orangeFlag').value = orangeFlag;
 }
 
 
 function orangePriceeMinus(){
     yourAmount =  document.querySelector('.yourAmountInput').value;
 
-    console.log('yourAmount '+yourAmount)
     if(yourAmount == 0){
-        console.log('you dont have money');
+        alert('   You Dont Have Money   ');
     }else if(totalOrangePrice < 15){
-        console.log('you dont have apple money');
+        alert('   You Dont Have Orange   ');
     }
     else{
+        orangeFlag--;
         totalOrangePrice = totalOrangePrice - orangePrice; 
     totalBuy = totalBuy - orangePrice;
-
-    console.log('minusApple '+totalOrangePrice)
-    console.log('minusApple '+totalBuy)
 }
-}
+document.querySelector('.totalBuy').innerHTML = totalBuy;
+document.querySelector('.orangeFlag').value = orangeFlag;
 
-// seprate
+}
 
 // seprate banana
 function bananaPriceAdd(){
     yourAmount =  document.querySelector('.yourAmountInput').value;
 
-    console.log('yourAmount '+yourAmount)
     if(yourAmount == 0){
-        console.log('you dont have money');
+        alert('   You Dont Have Money   ');
     }else{
+        bananaFlag++;
     totalBananaPrice = totalBananaPrice + bananaPrice; 
     totalBuy = totalBuy + bananaPrice;
 
-    console.log('added '+totalBananaPrice)
-    console.log('added '+totalBuy)
-
     if(totalBuy>yourAmount){
+        bananaFlag--;
         totalBananaPrice = totalBananaPrice - bananaPrice; 
         totalBuy = totalBuy - bananaPrice;
-
-        console.log('minus '+totalBananaPrice)
-        console.log('minus '+totalBuy)
-        console.log('you cross the limit')
+        alert('   You Cross The Limit   ')
         }
     }
-}
+    document.querySelector('.totalBuy').innerHTML = totalBuy;
+    document.querySelector('.bananaFlag').value = bananaFlag;
 
+}
 
 function bananaPriceMinus(){
     yourAmount =  document.querySelector('.yourAmountInput').value;
 
-    console.log('yourAmount '+yourAmount)
     if(yourAmount == 0){
-        console.log('you dont have money');
+        alert('   You Dont Have Money   ');
     }else if(totalBananaPrice < 7){
-        console.log('you dont have apple money');
+        alert('   You Dont Have Banana   ');
     }
     else{
+        bananaFlag--;
     totalBananaPrice = totalBananaPrice - bananaPrice; 
     totalBuy = totalBuy - bananaPrice;
-
-    console.log('minusBanana '+totalBananaPrice)
-    console.log('minusBanana '+totalBuy)
 }
+document.querySelector('.totalBuy').innerHTML = totalBuy;
+document.querySelector('.bananaFlag').value = bananaFlag;
+
 }
-
-// seprate
-
